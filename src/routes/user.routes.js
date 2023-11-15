79d47a02886ad5c8a6ca364186e665c389c45860
diego delgado <diegodelgado045@hotmail.com>
2023-11-15 01:35:26 -0500

    git init and REST API of users

---
+++ src/routes/user.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import {getUsers, getUser, createUser, updateUser, deleteUser} from '../controllers/user.controller.js'
+
+const router = Router()
+
+router.get("/user", getUsers)
+router.get("/user/:id", getUser)
+router.post("/user", createUser)
+router.patch("/user/:id", updateUser)
+router.delete("/user/:id", deleteUser)
+
+export default router
